e93cfdd3efb6507f13287a8389ffdeb62d95f2f9
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-14 11:01:15 -0400

    onsearchchange issue

---
+++ user.js
@@ -0,0 +1,30 @@
+const postListEl = document.querySelector(`.post-list`)
+
+function onSearchChange(event) {
+console.log(event.target.value)
+}
+
+async function main() {
+    const id = localStorage.getItem("id")
+    const posts = await fetch(
+      `https://jsonplaceholder.typicode.com/posts`
+    );
+    const postsData = await posts.json();
+    console.log(postsData)
+
+    postListEl.innerHTML = postsData
+      .map(
+        post =>
+          `<div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`
+      )
+      .join('');
+}
+
+main();
