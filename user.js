a235c390ff9f3e93fa194a3e629652dfd509eb86
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-15 12:21:45 -0400

    prep finished

--- user.js
+++ user.js
@@ -1,21 +1,24 @@
 const postListEl = document.querySelector(`.post-list`)
+const id = localStorage.getItem("id");
 
-function onSearchChange(event) {
-console.log(event.target.value)
+async function onSearchChange(event) {
+  const id = event.target.value;
+   renderPosts(id)
 }
 
-async function main() {
-    const id = localStorage.getItem("id")
+async function renderPosts(userId) {
     const posts = await fetch(
-      `https://jsonplaceholder.typicode.com/posts`
+        `https://jsonplaceholder.typicode.com/posts${userId || id}`
     );
     const postsData = await posts.json();
-    console.log(postsData)
+console.log(posts)
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
 
-    postListEl.innerHTML = postsData
-      .map(
-        post =>
-          `<div class="post">
+
+function postHTML(post) {
+  return
+    `<div class="post">
       <div class="post__title">
         ${post.title}
       </div>
@@ -23,8 +26,6 @@ async function main() {
         ${post.body}
       </p>
     </div>`
-      )
-      .join('');
 }
 
-main();
+renderPosts(id);
